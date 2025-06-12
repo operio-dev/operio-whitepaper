88c0f48754a095c6ecd67cbe09f11f097f9fd56b
operio-dev <ftandassociates@gmail.com>
2025-06-13 00:01:34 +0200

    Create sidebars.js

---
+++ sidebars.js
@@ -0,0 +1,14 @@
+module.exports = {
+  docs: [
+    '01_introduction',
+    '02_market_problem',
+    '03_architecture',
+    '04_product_modules',
+    '05_technology_stack',
+    '06_scalability_security',
+    '07_roadmap',
+    '08_revenue_model',
+    '09_team_advisors',
+    '10_faq',
+  ],
+};
