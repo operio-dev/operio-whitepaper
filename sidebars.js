aa9cc820156e93848fa8f54285a36e92911790f5
operio-dev <ftandassociates@gmail.com>
2025-06-14 00:25:31 +0200

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -1,15 +1,17 @@
-module.exports = {
-  docs: [
+{
+  type: 'category',
+  label: 'Whitepaper',
+  items: [
     '01_introduction',
     '02_market_problem',
     '03_problem_validation_problem_impact',
     '04_architecture',
     '05_product_modules',
-    '06_technology_stack',
+    '06_technology_stack',          // <-- questo deve esserci!
     '07_scalability_security',
     '08_roadmap',
     '09_revenue_model',
     '10_team_advisors',
-    '11_faq',
-  ],
-};
+    '11_faq'
+  ]
+}
